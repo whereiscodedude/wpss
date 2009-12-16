861215ca42428a2f08db94e60559bd5751ce069f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-12-16 17:55:36 +0000

    2.9 branch, from [12416]

---
+++ branches/2.9/wp-includes/version.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This holds the version number in a separate file so we can bump it without cluttering the SVN
+ */
+
+/**
+ * The WordPress version string
+ *
+ * @global string $wp_version
+ */
+$wp_version = '2.9-RC1';
+
+/**
+ * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
+ *
+ * @global int $wp_db_version
+ */
+$wp_db_version = 12329;
+
+/**
+ * Holds the TinyMCE version
+ *
+ * @global string $tinymce_version
+ */
+$tinymce_version = '327-1235';
+
+/**
+ * Holds the cache manifest version
+ *
+ * @global string $manifest_version
+ */
+$manifest_version = '20090616';
