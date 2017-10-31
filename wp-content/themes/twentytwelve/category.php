95041c3f90d53a0ac616e526f849b066bccc9515
swissspidy <swissspidy@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2017-10-31 15:52:08 +0000

    Tag 4.0.20 Built from https://develop.svn.wordpress.org/tags/4.0.20@42089

--- wp-content/themes/twentytwelve/category.php
+++ wp-content/themes/twentytwelve/category.php
@@ -4,7 +4,7 @@
  *
  * Used to display archive-type pages for posts in a category.
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
  * @subpackage Twenty_Twelve
@@ -30,7 +30,7 @@ get_header(); ?>
 			while ( have_posts() ) : the_post();
 
 				/* Include the post format-specific template for the content. If you want to
-				 * this in a child theme then include a file called content-___.php
+				 * this in a child theme then include a file called called content-___.php
 				 * (where ___ is the post format) and that will be used instead.
 				 */
 				get_template_part( 'content', get_post_format() );
