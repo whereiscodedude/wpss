40afb095b21c477ae7a1367ce4477620415313fa
jeremyfelt <jeremyfelt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2018-12-13 03:32:34 +0000

    Tag 4.5.16 Built from https://develop.svn.wordpress.org/tags/4.5.16@44096

--- wp-content/themes/twentythirteen/js/functions.js
+++ wp-content/themes/twentythirteen/js/functions.js
@@ -75,11 +75,9 @@
 	} )();
 
 	/**
-	 * Add or remove ARIA attributes.
-	 *
+	 * @summary Add or remove ARIA attributes.
 	 * Uses jQuery's width() function to determine the size of the window and add
 	 * the default ARIA attributes for the menu toggle if it's visible.
-	 *
 	 * @since Twenty Thirteen 1.5
 	 */
 	function onResizeARIA() {
@@ -121,13 +119,9 @@
 	/**
 	 * Arranges footer widgets vertically.
 	 */
-	$( function() {
-		var columnWidth, widgetArea;
-		if ( ! $.isFunction( $.fn.masonry ) ) {
-			return;
-		}
-		columnWidth = body.is( '.sidebar' ) ? 228 : 245;
-		widgetArea = $( '#secondary .widget-area' );
+	if ( $.isFunction( $.fn.masonry ) ) {
+		var columnWidth = body.is( '.sidebar' ) ? 228 : 245,
+			widgetArea = $( '#secondary .widget-area' );
 
 		widgetArea.masonry( {
 			itemSelector: '.widget',
@@ -163,5 +157,5 @@
 				}
 			} );
 		}
-	} );
+	}
 } )( jQuery );
