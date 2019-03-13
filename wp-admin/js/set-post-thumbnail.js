48cc090660561dff415ab6005006c166c96b7575
pento <pento@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2019-03-13 03:14:16 +0000

    Tag 3.7.29 Built from https://develop.svn.wordpress.org/tags/3.7.29@44893

--- wp-admin/js/set-post-thumbnail.js
+++ wp-admin/js/set-post-thumbnail.js
@@ -1,16 +1,9 @@
-/**
- * @output wp-admin/js/set-post-thumbnail.js
- */
-
-/* global setPostThumbnailL10n, ajaxurl, post_id, alert */
-/* exported WPSetAsThumbnail */
-
-window.WPSetAsThumbnail = function( id, nonce ) {
+function WPSetAsThumbnail(id, nonce){
 	var $link = jQuery('a#wp-post-thumbnail-' + id);
 
 	$link.text( setPostThumbnailL10n.saving );
 	jQuery.post(ajaxurl, {
-		action: 'set-post-thumbnail', post_id: post_id, thumbnail_id: id, _ajax_nonce: nonce, cookie: encodeURIComponent( document.cookie )
+		action:"set-post-thumbnail", post_id: post_id, thumbnail_id: id, _ajax_nonce: nonce, cookie: encodeURIComponent(document.cookie)
 	}, function(str){
 		var win = window.dialogArguments || opener || parent || top;
 		$link.text( setPostThumbnailL10n.setThumbnail );
@@ -25,4 +18,4 @@ window.WPSetAsThumbnail = function( id, nonce ) {
 		}
 	}
 	);
-};
+}
