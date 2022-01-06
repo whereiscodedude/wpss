5d05fb1493c077257955bf7e379510358c8640ab
desrosj <desrosj@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2022-01-06 20:45:52 +0000

    Tag 3.7.37 Built from https://develop.svn.wordpress.org/tags/3.7.37@52531

--- wp-includes/js/wp-util.js
+++ wp-includes/js/wp-util.js
@@ -1,10 +1,3 @@
-/**
- * @output wp-includes/js/wp-util.js
- */
-
-/* global _wpUtilSettings */
-
-/** @namespace wp */
 window.wp = window.wp || {};
 
 (function ($) {
@@ -14,20 +7,14 @@ window.wp = window.wp || {};
 	/**
 	 * wp.template( id )
 	 *
-	 * Fetch a JavaScript template for an id, and return a templating function for it.
+	 * Fetches a template by id.
 	 *
-	 * @param {string} id A string that corresponds to a DOM element with an id prefixed with "tmpl-".
-	 *                    For example, "attachment" maps to "tmpl-attachment".
-	 * @return {function} A function that lazily-compiles the template requested.
+	 * @param  {string} id   A string that corresponds to a DOM element with an id prefixed with "tmpl-".
+	 *                       For example, "attachment" maps to "tmpl-attachment".
+	 * @return {function}    A function that lazily-compiles the template requested.
 	 */
 	wp.template = _.memoize(function ( id ) {
 		var compiled,
-			/*
-			 * Underscore's default ERB-style templates are incompatible with PHP
-			 * when asp_tags is enabled, so WordPress uses Mustache-inspired templating syntax.
-			 *
-			 * @see trac ticket #22344.
-			 */
 			options = {
 				evaluate:    /<#([\s\S]+?)#>/g,
 				interpolate: /\{\{\{([\s\S]+?)\}\}\}/g,
@@ -36,18 +23,16 @@ window.wp = window.wp || {};
 			};
 
 		return function ( data ) {
-			compiled = compiled || _.template( $( '#tmpl-' + id ).html(),  options );
+			compiled = compiled || _.template( $( '#tmpl-' + id ).html(), null, options );
 			return compiled( data );
 		};
 	});
 
-	/*
-	 * wp.ajax
-	 * ------
-	 *
-	 * Tools for sending ajax requests with JSON responses and built in error handling.
-	 * Mirrors and wraps jQuery's ajax APIs.
-	 */
+	// wp.ajax
+	// ------
+	//
+	// Tools for sending ajax requests with JSON responses and built in error handling.
+	// Mirrors and wraps jQuery's ajax APIs.
 	wp.ajax = {
 		settings: settings.ajax || {},
 
@@ -56,11 +41,9 @@ window.wp = window.wp || {};
 		 *
 		 * Sends a POST request to WordPress.
 		 *
-		 * @param {(string|Object)} action The slug of the action to fire in WordPress or options passed
-		 *                                 to jQuery.ajax.
-		 * @param {Object=}         data   Optional. The data to populate $_POST with.
-		 * @return {$.promise} A jQuery promise that represents the request,
-		 *                     decorated with an abort() method.
+		 * @param  {string} action The slug of the action to fire in WordPress.
+		 * @param  {object} data   The data to populate $_POST with.
+		 * @return {$.promise}     A jQuery promise that represents the request.
 		 */
 		post: function( action, data ) {
 			return wp.ajax.send({
@@ -73,14 +56,11 @@ window.wp = window.wp || {};
 		 *
 		 * Sends a POST request to WordPress.
 		 *
-		 * @param {(string|Object)} action  The slug of the action to fire in WordPress or options passed
-		 *                                  to jQuery.ajax.
-		 * @param {Object=}         options Optional. The options passed to jQuery.ajax.
-		 * @return {$.promise} A jQuery promise that represents the request,
-		 *                     decorated with an abort() method.
+		 * @param  {string} action  The slug of the action to fire in WordPress.
+		 * @param  {object} options The options passed to jQuery.ajax.
+		 * @return {$.promise}      A jQuery promise that represents the request.
 		 */
 		send: function( action, options ) {
-			var promise, deferred;
 			if ( _.isObject( action ) ) {
 				options = action;
 			} else {
@@ -94,59 +74,31 @@ window.wp = window.wp || {};
 				context: this
 			});
 
-			deferred = $.Deferred( function( deferred ) {
+			return $.Deferred( function( deferred ) {
 				// Transfer success/error callbacks.
-				if ( options.success ) {
+				if ( options.success )
 					deferred.done( options.success );
-				}
-
-				if ( options.error ) {
+				if ( options.error )
 					deferred.fail( options.error );
-				}
 
 				delete options.success;
 				delete options.error;
 
-				// Use with PHP's wp_send_json_success() and wp_send_json_error().
-				deferred.jqXHR = $.ajax( options ).done( function( response ) {
-					// Treat a response of 1 as successful for backward compatibility with existing handlers.
-					if ( response === '1' || response === 1 ) {
+				// Use with PHP's wp_send_json_success() and wp_send_json_error()
+				$.ajax( options ).done( function( response ) {
+					// Treat a response of `1` as successful for backwards
+					// compatibility with existing handlers.
+					if ( response === '1' || response === 1 )
 						response = { success: true };
-					}
 
-					if ( _.isObject( response ) && ! _.isUndefined( response.success ) ) {
-
-						// When handling a media attachments request, get the total attachments from response headers.
-						var context = this;
-						deferred.done( function() {
-							if (
-								action &&
-								action.data &&
-								'query-attachments' === action.data.action &&
-								deferred.jqXHR.hasOwnProperty( 'getResponseHeader' ) &&
-								deferred.jqXHR.getResponseHeader( 'X-WP-Total' )
-							) {
-								context.totalAttachments = parseInt( deferred.jqXHR.getResponseHeader( 'X-WP-Total' ), 10 );
-							} else {
-								context.totalAttachments = 0;
-							}
-						} );
+					if ( _.isObject( response ) && ! _.isUndefined( response.success ) )
 						deferred[ response.success ? 'resolveWith' : 'rejectWith' ]( this, [response.data] );
-					} else {
+					else
 						deferred.rejectWith( this, [response] );
-					}
 				}).fail( function() {
 					deferred.rejectWith( this, arguments );
 				});
-			});
-
-			promise = deferred.promise();
-			promise.abort = function() {
-				deferred.jqXHR.abort();
-				return this;
-			};
-
-			return promise;
+			}).promise();
 		}
 	};
 
